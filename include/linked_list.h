7fd2dd6e205b58bd2fc0e83e85b6e5352831649f
au706600 <202106601@post.au.dk>
2021-12-07 14:51:28 +0100

    implemented linked_list.h

--- include/linked_list.h
+++ include/linked_list.h
@@ -8,7 +8,7 @@ typedef struct node
 } node;
 
 node *make_node (int v, node * q);
-void free_list (node * p);
+void free_list (node **p);
 void print_list (node * p);
 int sum_squares (node * p);
 node *map (node * p, int (*f) (int));
